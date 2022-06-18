7ed4ae6713ea4573fc344ccac86a355630c5a415
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2022-06-18 12:04:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,24 +8,24 @@ global.APIs = {
 	zenz: 'https://zenzapis.xyz',
 }
 
-//Api Key\\
+
 global.APIKeys = {
-	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+	'https://zenzapis.xyz': 'sanzychan01', 
 }
 
-//customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
+
+global.owner = ['94771039631']
+global.premium = ['94771039631']
+global.ownernomer = '94771039631'
+global.ownername = 'Hiruwa'
+global.botname = 'Anonymous'
+global.footer = '©Anonymous'
+global.ig = 'https://github.com/'
 global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.sc = 'https://github.com/'
+global.myweb = 'https://youtube.com/'
+global.packname = 'Anonymous'
+global.author = 'Anonymous'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
